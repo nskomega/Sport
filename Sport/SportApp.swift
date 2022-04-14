ee63118eac548f931a705fa60db63bcd58de6449
Mikhail Danilov <2588000nsk@gmail.com>
2022-04-14 04:47:48 +0300

    Add OneSignal

--- Sport/SportApp.swift
+++ Sport/SportApp.swift
@@ -6,12 +6,33 @@
 //
 
 import SwiftUI
+import OneSignal
 
 @main
 struct SportApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+       // Remove this method to stop OneSignal Debugging
+       OneSignal.setLogLevel(.LL_VERBOSE, visualLevel: .LL_NONE)
+
+       OneSignal.initWithLaunchOptions(launchOptions)
+       OneSignal.setAppId("YOUR_ONESIGNAL_APP_ID")
+
+       OneSignal.promptForPushNotifications(userResponse: { accepted in
+         print("User accepted notification: \(accepted)")
+       })
+
+      // Set your customer userId
+      // OneSignal.setExternalUserId("userId")
+
+       return true
     }
 }
