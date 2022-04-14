aada0b311dc54bbd58325e644e6fb4c81d2ccdb2
Mikhail Danilov <2588000nsk@gmail.com>
2022-04-14 04:18:20 +0300

    Create Project

---
+++ Sport/SportApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportApp.swift
+//  Sport
+//
+//  Created by Mikhail Danilov on 14.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
